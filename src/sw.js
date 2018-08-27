f7f4337f5f4bb3391603e44fa52ed7edc20d124f
Ozaki Yuichi <ozaki.yuichi@sample.com>
2018-08-27 11:16:52 +0900

    add all file to cache

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
-var CACHE_NAME = 'pwa-sample';
-var urlsToCache = [
+const CACHE_NAME = 'pwa-sample';
+const urlsToCache = [
   '/assets/css/0.styles.7775a0d8.css',
   '/assets/js/2.cda51ec0.js',
   '/assets/js/2.cda51ec0.js',
@@ -12,7 +12,35 @@ var urlsToCache = [
   '/assets/js/9.8c068cc8.js',
   '/assets/js/10.a7cb3a8f.js',
   '/assets/js/11.500572b9.js',
-  '/assets/js/app.e796080a.js'
+  '/assets/js/app.e796080a.js',
+  '/assets/images/1/hotreload.gif',
+  '/assets/images/1/template.png',
+  '/assets/images/2/hello.png',
+  '/assets/images/2/reactdom.png',
+  '/assets/images/3/hello.png',
+  '/assets/images/3/hello2.png',
+  '/assets/images/3/hello3.png',
+  '/assets/images/4/counter.gif',
+  '/assets/images/4/counter.png',
+  '/assets/images/4/counter2.gif',
+  '/assets/images/5/todo_console.png',
+  '/assets/images/5/todo.png',
+  '/assets/images/5/todo2.png',
+  '/assets/images/5/todo3.gif',
+  '/assets/images/5/todo4.png',
+  '/assets/images/5/todo5.gif',
+  '/assets/images/6/router.gif',
+  '/assets/images/6/router2.gif',
+  '/assets/images/6/router3.gif',
+  '1_setup.html',
+  '2_initialfile.html',
+  '3_hello.html',
+  '4_counter.html',
+  '5_todolist.html',
+  '6_routing.html',
+  '7_free.html',
+  '404.html',
+  'index.html',
 ];
 
 self.addEventListener('install', function(event) {
@@ -25,6 +53,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  console.log(event);
   event.respondWith(
     caches.match(event.request).then(function(response) {
       if (response) {
@@ -32,10 +61,12 @@ self.addEventListener('fetch', function(event) {
         return response;
       }
       console.log('not exist in cache');
-      return fetch(event.request).then(function(response) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
+      return fetch(event.request)
+        .then(function(response) {
+          console.log(response.url);
+          return response;
+        })
+        .catch(console.log);
     })
   );
 });
