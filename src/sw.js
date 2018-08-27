0ccf8ecabc7774d95175e3dec422b15c2bd36c3a
Ozaki Yuichi <ozaki.yuichi@sample.com>
2018-08-27 09:30:03 +0900

    add cache js

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,19 @@
 var CACHE_NAME = 'pwa-sample';
-var urlsToCache = ['/', '/assets/css/0.styles.7775a0d8.css'];
+var urlsToCache = [
+  '/assets/css/0.styles.7775a0d8.css',
+  '/assets/js/2.cda51ec0.js',
+  '/assets/js/2.cda51ec0.js',
+  '/assets/js/3.9e80b1cb.js',
+  '/assets/js/4.e8f43cf9.js',
+  '/assets/js/5.9742906c.js',
+  '/assets/js/6.45d077d6.js',
+  '/assets/js/7.3d93238c.js',
+  '/assets/js/8.04203a0c.js',
+  '/assets/js/9.8c068cc8.js',
+  '/assets/js/10.a7cb3a8f.js',
+  '/assets/js/11.500572b9.js',
+  '/assets/js/app.e796080a.js'
+];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -18,7 +32,10 @@ self.addEventListener('fetch', function(event) {
         return response;
       }
       console.log('not exist in cache');
-      return fetch(event.request);
+      return fetch(event.request).then(function(response) {
+        cache.put(event.request, response.clone());
+        return response;
+      });
     })
   );
 });
